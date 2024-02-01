37dfc928dce119f98bf25093275f1dd5724379d5
Crowdbotics <team@crowdbotics.com>
2024-02-01 20:12:08 +0000

    Initial commit

---
+++ backend/ndva_2_45750/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for ndva_2_45750 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "ndva_2_45750.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ndva-2-45750",
+  url: "https://ndva-2-45750.botics.co",
+  api: "https://ndva-2-45750.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
